9ee6d2c9946d463875906add846c0a9b8e22bfdc
Nachiketa Tiwari <nachiketatiwari56@gmail.com>
2024-08-27 18:55:06 +0530

    first commit

--- todolist/app/page.js
+++ todolist/app/page.js
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client"
+import React,{useState} from "react";
+import styles from "./page.css";
 
 const page = () => {
+
+  const [task, settask] = useState("")
+
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div className="mainContainer">
+        <h1>Todo App</h1>
+        <form>
+          <div className="upper">
+            <input
+              className="taskbox"
+              type="text"
+              placeholder="Add a new task"
+              value={task}
+            ></input>
+            <button className="taskadd">+</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default page;
